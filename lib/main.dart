1d490be0c8dcdcff38cd7f890ee9a63f8614c03d
Wasim Ishaq Khan <wasimwik919@gmail.com>
2021-11-05 01:38:34 +0530

    day5 done

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,23 @@
 import "package:flutter/material.dart";
+import "package:google_fonts/google_fonts.dart";
+
 import 'package:new_apps/pages/home_page.dart';
 import 'package:new_apps/pages/login_page.dart';
+import 'package:new_apps/utils/routes.dart';
 
 void main() {
   runApp(
     MaterialApp(
         themeMode: ThemeMode.light,
-        theme: ThemeData(primarySwatch: Colors.deepPurple),
+        theme: ThemeData(
+          primarySwatch: Colors.deepPurple,
+          fontFamily: GoogleFonts.lato().fontFamily,
+        ),
         darkTheme: ThemeData(brightness: Brightness.dark),
         routes: {
           "/": (context) => const LoginPage(),
-          "/home": (context) => const HomePage(),
-          "/login": (context) => const LoginPage()
+          MyRoutes.homeRoute: (context) => const HomePage(),
+          MyRoutes.loginRoute: (context) => const LoginPage()
         }),
   );
 }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import 'package:new_apps/utils/routes.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -12,46 +13,65 @@ class _LoginPageState extends State<LoginPage> {
   final _passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Test App"),
-      ),
-      body: SingleChildScrollView(
+    return Material(
+      // appBar: AppBar(
+      //   title: const Text("Test App"),
+      // ),
+      // backgroundColor: Colors.white,
+      color: Colors.white,
+      child: SingleChildScrollView(
         child: Column(
           children: [
-            Image.asset("assets/images/login_image.png"),
+            SizedBox(
+              height: MediaQuery.of(context).padding.top,
+            ),
+            Image.asset("assets/images/login_image.png", fit: BoxFit.cover),
             const SizedBox(height: 20.0),
             Padding(
               padding:
-                  const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+                  const EdgeInsets.symmetric(horizontal: 32.0, vertical: 16.0),
               child: Form(
                 child: Column(
                   children: [
+                    const Text(
+                      "Welcome",
+                      style: TextStyle(
+                        fontSize: 28,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 20.0),
                     TextFormField(
                       controller: _usernameController,
                       decoration: const InputDecoration(
                           labelText: "Enter Username", hintText: "Username"),
                     ),
-                    const SizedBox(height: 16.0),
+                    const SizedBox(height: 20.0),
                     TextFormField(
                       controller: _passwordController,
                       decoration: const InputDecoration(
                           labelText: "Enter Password", hintText: "Password"),
+                      obscureText: true,
                     ),
-                    const SizedBox(height: 16.0),
+                    const SizedBox(height: 40.0),
                     Padding(
                       padding: const EdgeInsets.symmetric(
                           vertical: 15.0, horizontal: 30.0),
                       child: ElevatedButton(
                         onPressed: () {
-                          print("hi");
+                          Navigator.pushNamed(context, MyRoutes.homeRoute);
+                          // print("hi");
                         },
+                        style: TextButton.styleFrom(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 12.0, horizontal: 42.0),
+                        ),
                         child: const Text(
-                          "Submit",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20.0),
+                          "Login",
+                          // style: TextStyle(
+                          //     color: Colors.white,
+                          //     fontWeight: FontWeight.bold,
+                          //     fontSize: 20.0),
                         ),
                       ),
                     )
