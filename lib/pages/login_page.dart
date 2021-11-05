0abe25c73f24881eb5b512ecb4d8cbcc7f256594
Wasim Ishaq Khan <wasimwik919@gmail.com>
2021-11-05 19:01:42 +0530

    day6 completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -11,6 +11,8 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   final _usernameController = TextEditingController();
   final _passwordController = TextEditingController();
+  bool _isTouched = false;
+
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -55,26 +57,65 @@ class _LoginPageState extends State<LoginPage> {
                     ),
                     const SizedBox(height: 40.0),
                     Padding(
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 15.0, horizontal: 30.0),
-                      child: ElevatedButton(
-                        onPressed: () {
-                          Navigator.pushNamed(context, MyRoutes.homeRoute);
-                          // print("hi");
-                        },
-                        style: TextButton.styleFrom(
-                          padding: const EdgeInsets.symmetric(
-                              vertical: 12.0, horizontal: 42.0),
-                        ),
-                        child: const Text(
-                          "Login",
-                          // style: TextStyle(
-                          //     color: Colors.white,
-                          //     fontWeight: FontWeight.bold,
-                          //     fontSize: 20.0),
-                        ),
-                      ),
-                    )
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 15.0, horizontal: 30.0),
+                        child: InkWell(
+                          onTap: () async {
+                            setState(() {
+                              _isTouched = true;
+                            });
+                            await Future.delayed(const Duration(seconds: 1))
+                                .then((value) {
+                              Navigator.pushNamed(context, MyRoutes.homeRoute);
+                              // setState(() {
+                              //   _isTouched = false;
+                              // });
+                            });
+                          },
+                          child: AnimatedContainer(
+                            duration: const Duration(seconds: 1),
+                            width: _isTouched ? 50 : 150,
+                            height: 50,
+                            decoration: BoxDecoration(
+                              color: Colors.deepPurpleAccent,
+                              borderRadius: _isTouched
+                                  ? BorderRadius.circular(25.0)
+                                  : BorderRadius.circular(8.0),
+                            ),
+                            alignment: Alignment.center,
+                            child: _isTouched
+                                ? const Icon(
+                                    Icons.done,
+                                    color: Colors.white,
+                                  )
+                                : const Text(
+                                    "Login",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 18.0,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                          ),
+                        )
+                        // ElevatedButton(
+                        //   onPressed: () {
+                        //     Navigator.pushNamed(context, MyRoutes.homeRoute);
+                        //     // print("hi");
+                        //   },
+                        //   style: TextButton.styleFrom(
+                        //     padding: const EdgeInsets.symmetric(
+                        //         vertical: 12.0, horizontal: 42.0),
+                        //   ),
+                        //   child: const Text(
+                        //     "Login",
+                        //     // style: TextStyle(
+                        //     //     color: Colors.white,
+                        //     //     fontWeight: FontWeight.bold,
+                        //     //     fontSize: 20.0),
+                        //   ),
+                        // ),
+                        )
                   ],
                 ),
               ),
